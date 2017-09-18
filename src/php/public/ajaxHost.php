710b5b4c83eb743d23eea974f4869e14634156ae
Lee Baird <leebaird@gmail.com>
2017-09-18 18:18:48 -0400

    Added 2 new Ajax files.

---
+++ src/php/public/ajaxHost.php
@@ -0,0 +1,18 @@
+<?php
+    $sn="";
+    $username = "root";
+    $password = "";
+    $host = "localhost";
+    $dbname = "amdb";
+    $connection = new mysqli($host,$username,$password,$dbname);
+    $query = "SELECT * FROM clients WHERE clientID=".$_POST['country_id'];
+    $result = mysqli_query($connection, $query);
+    $row = mysqli_fetch_array($result);
+    $sn .= $row['address']." ";
+    $sn .= $row['state']." ";
+    $sn .= $row['zip']." ";
+    $sn .= $row['phone']." ";
+    $sn .= $row['web']." ";
+	
+    print $sn;
+?>
---
+++ src/php/public/ajaxWeb.php
@@ -0,0 +1,17 @@
+<?php
+    $sn="";
+    $username = "root";
+    $password = "";
+    $host = "localhost";
+    $dbname = "amdb";
+    $connection = new mysqli($host,$username,$password,$dbname);
+    $query = "SELECT * FROM findings WHERE findingID=".$_POST['findingID'];
+    $result = mysqli_query($connection, $query);
+    $row = mysqli_fetch_array($result);
+    $sn .= $row['severity']." ";
+    $sn .= $row['observation']." ";
+    $sn .= $row['remediation']." ";
+    $sn .= $row['see_also']." ";
+	
+    print $sn;
+?>
