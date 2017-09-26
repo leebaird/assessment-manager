39df36c7801f7164cf9fec3576416a3dc9970559
Lee Baird <leebaird@gmail.com>
2017-09-26 13:06:05 -0400

    Updated flat db.

--- src/db/mysql/amdb/flat.sql
+++ src/db/mysql/amdb/flat.sql
@@ -34,14 +34,14 @@ CREATE TABLE `clients` (
   `clientID` int(4) NOT NULL,
   `modified` datetime NOT NULL,
   `client` varchar(50) COLLATE utf8_bin NOT NULL,
-  `address` varchar(50) COLLATE utf8_bin NOT NULL,
-  `city` varchar(25) COLLATE utf8_bin NOT NULL,
-  `state` varchar(2) COLLATE utf8_bin NOT NULL,
-  `zip` varchar(10) COLLATE utf8_bin NOT NULL,
-  `phone` varchar(20) COLLATE utf8_bin NOT NULL,
-  `web` varchar(50) COLLATE utf8_bin NOT NULL,
-  `employeeID` int(4) NOT NULL,
-  `notes` text COLLATE utf8_bin NOT NULL
+  `address` varchar(50) COLLATE utf8_bin,
+  `city` varchar(25) COLLATE utf8_bin,
+  `state` varchar(2) COLLATE utf8_bin,
+  `zip` varchar(10) COLLATE utf8_bin,
+  `phone` varchar(20) COLLATE utf8_bin,
+  `web` varchar(50) COLLATE utf8_bin,
+  `employeeID` int(4),
+  `notes` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -64,11 +64,11 @@ CREATE TABLE `contacts` (
   `contactID` int(4) NOT NULL,
   `modified` datetime NOT NULL,
   `contact` varchar(50) COLLATE utf8_bin NOT NULL,
-  `title` varchar(50) COLLATE utf8_bin NOT NULL,
-  `work` varchar(20) COLLATE utf8_bin NOT NULL,
-  `cell` varchar(12) COLLATE utf8_bin NOT NULL,
-  `email` varchar(50) COLLATE utf8_bin NOT NULL,
-  `notes` text COLLATE utf8_bin NOT NULL,
+  `title` varchar(50) COLLATE utf8_bin,
+  `work` varchar(20) COLLATE utf8_bin,
+  `cell` varchar(12) COLLATE utf8_bin,
+  `email` varchar(50) COLLATE utf8_bin,
+  `notes` text COLLATE utf8_bin,
   `clientID` int(4) NOT NULL,
   `projectID` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -93,12 +93,11 @@ CREATE TABLE `employees` (
   `employeeID` int(4) NOT NULL,
   `modified` datetime NOT NULL,
   `employee` varchar(50) COLLATE utf8_bin NOT NULL,
-  `title` varchar(25) COLLATE utf8_bin NOT NULL,
-  `type` varchar(10) COLLATE utf8_bin NOT NULL,
-  `accountmgr` varchar(3) COLLATE utf8_bin NOT NULL,
-  `cell` varchar(12) COLLATE utf8_bin NOT NULL,
-  `email` varchar(50) COLLATE utf8_bin NOT NULL,
-  `notes` text COLLATE utf8_bin NOT NULL,
+  `title` varchar(25) COLLATE utf8_bin,
+  `accountmgr` varchar(3) COLLATE utf8_bin,
+  `cell` varchar(12) COLLATE utf8_bin,
+  `email` varchar(50) COLLATE utf8_bin,
+  `notes` text COLLATE utf8_bin,
   `projectID` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -106,12 +105,12 @@ CREATE TABLE `employees` (
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`employeeID`, `modified`, `employee`, `title`, `type`, `accountmgr`, `cell`, `email`, `notes`, `projectID`) VALUES
-(1, '2017-09-18 20:53:41', 'Bugs Bunny', 'Manager', '', '', '', 'bugs.bunny@acme.com', '', 0),
-(2, '2017-09-18 20:54:40', 'Moe Howard', 'Red Team', '', '', '', '', 'Linux', 0),
-(3, '2017-09-18 20:55:02', 'Larry Fine', 'Red Team', '', '', '', '', 'Windows', 0),
-(4, '2017-09-18 20:55:27', 'Curly Howard', 'Red Team', '', '', '', '', 'Web', 0),
-(5, '2017-09-22 22:11:25', 'Wile E Coyote', 'Pure Genius', '', 'Yes', '', '', '', 0);
+INSERT INTO `employees` (`employeeID`, `modified`, `employee`, `title`, `accountmgr`, `cell`, `email`, `notes`, `projectID`) VALUES
+(1, '2017-09-18 20:53:41', 'Bugs Bunny', 'Manager', '', '', 'bugs.bunny@acme.com', '', 0),
+(2, '2017-09-18 20:54:40', 'Moe Howard', 'Red Team', '', '', '', 'Linux', 0),
+(3, '2017-09-18 20:55:02', 'Larry Fine', 'Red Team', '', '', '', 'Windows', 0),
+(4, '2017-09-18 20:55:27', 'Curly Howard', 'Red Team', '', '', '', 'Web', 0),
+(5, '2017-09-22 22:11:25', 'Wile E Coyote', 'Pure Genius', 'Yes', '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -137,10 +136,10 @@ CREATE TABLE `findings` (
   `modified` datetime NOT NULL,
   `type` varchar(25) COLLATE utf8_bin NOT NULL,
   `finding` varchar(50) COLLATE utf8_bin NOT NULL,
-  `observation` text COLLATE utf8_bin NOT NULL,
+  `observation` text COLLATE utf8_bin,
   `severity` text COLLATE utf8_bin NOT NULL,
-  `remediation` text COLLATE utf8_bin NOT NULL,
-  `see_also` text COLLATE utf8_bin NOT NULL
+  `remediation` text COLLATE utf8_bin,
+  `see_also` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -165,14 +164,14 @@ CREATE TABLE `hostvulns` (
   `tool` varchar(16) COLLATE utf8_bin NOT NULL,
   `vulnerability` varchar(128) COLLATE utf8_bin NOT NULL,
   `findingID` int(3) NOT NULL,
-  `cvss_base` int(2) NOT NULL,
-  `internal` varchar(8) COLLATE utf8_bin NOT NULL,
-  `external` varchar(8) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
-  `remediation` text COLLATE utf8_bin NOT NULL,
-  `see_also` text COLLATE utf8_bin NOT NULL,
-  `published` varchar(12) COLLATE utf8_bin NOT NULL,
-  `updated` varchar(12) COLLATE utf8_bin NOT NULL
+  `cvss_base` int(2),
+  `internal` varchar(8) COLLATE utf8_bin,
+  `external` varchar(8) COLLATE utf8_bin,
+  `description` text COLLATE utf8_bin,
+  `remediation` text COLLATE utf8_bin,
+  `see_also` text COLLATE utf8_bin,
+  `published` varchar(12) COLLATE utf8_bin,
+  `updated` varchar(12) COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -195,27 +194,27 @@ CREATE TABLE `projects` (
   `project` varchar(50) COLLATE utf8_bin NOT NULL,
   `assessment` varchar(255) COLLATE utf8_bin NOT NULL,
   `client` varchar(50) COLLATE utf8_bin NOT NULL,
-  `address` varchar(25) COLLATE utf8_bin NOT NULL,
-  `city` varchar(25) COLLATE utf8_bin NOT NULL,
-  `state` varchar(2) COLLATE utf8_bin NOT NULL,
-  `zip` varchar(10) COLLATE utf8_bin NOT NULL,
-  `phone` varchar(20) COLLATE utf8_bin NOT NULL,
-  `web` varchar(50) COLLATE utf8_bin NOT NULL,
+  `address` varchar(25) COLLATE utf8_bin,
+  `city` varchar(25) COLLATE utf8_bin,
+  `state` varchar(2) COLLATE utf8_bin,
+  `zip` varchar(10) COLLATE utf8_bin,
+  `phone` varchar(20) COLLATE utf8_bin,
+  `web` varchar(50) COLLATE utf8_bin,
   `employeeID` varchar(4) COLLATE utf8_bin NOT NULL,
-  `projectmgr` varchar(50) COLLATE utf8_bin NOT NULL,
-  `consultant1` varchar(50) COLLATE utf8_bin NOT NULL,
-  `consultant2` varchar(50) COLLATE utf8_bin NOT NULL,
-  `consultant3` varchar(50) COLLATE utf8_bin NOT NULL,
-  `consultant4` varchar(50) COLLATE utf8_bin NOT NULL,
-  `kickoff` varchar(12) COLLATE utf8_bin NOT NULL,
-  `start` varchar(12) COLLATE utf8_bin NOT NULL,
-  `tech_qa` varchar(12) COLLATE utf8_bin NOT NULL,
-  `draft_delivery` varchar(12) COLLATE utf8_bin NOT NULL,
-  `final_delivery` varchar(12) COLLATE utf8_bin NOT NULL,
-  `finish` varchar(12) COLLATE utf8_bin NOT NULL,
-  `status` varchar(10) COLLATE utf8_bin NOT NULL,
-  `notes` text COLLATE utf8_bin NOT NULL,
-  `objective` varchar(100) COLLATE utf8_bin NOT NULL
+  `projectmgr` varchar(50) COLLATE utf8_bin,
+  `consultant1` varchar(50) COLLATE utf8_bin,
+  `consultant2` varchar(50) COLLATE utf8_bin,
+  `consultant3` varchar(50) COLLATE utf8_bin,
+  `consultant4` varchar(50) COLLATE utf8_bin,
+  `kickoff` varchar(12) COLLATE utf8_bin,
+  `start` varchar(12) COLLATE utf8_bin,
+  `tech_qa` varchar(12) COLLATE utf8_bin,
+  `draft_delivery` varchar(12) COLLATE utf8_bin,
+  `final_delivery` varchar(12) COLLATE utf8_bin,
+  `finish` varchar(12) COLLATE utf8_bin,
+  `status` varchar(10) COLLATE utf8_bin,
+  `notes` text COLLATE utf8_bin,
+  `objective` varchar(100) COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -282,13 +281,13 @@ CREATE TABLE `vulnerabilities` (
   `vulnerabilityID` int(5) NOT NULL,
   `modified` datetime NOT NULL,
   `vulnerability` varchar(100) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
-  `solution` text COLLATE utf8_bin NOT NULL,
-  `cvss_base_score` decimal(3,1) NOT NULL,
-  `see_also` text COLLATE utf8_bin NOT NULL,
-  `cve` varchar(50) COLLATE utf8_bin NOT NULL,
-  `internal` varchar(10) COLLATE utf8_bin NOT NULL,
-  `external` varchar(10) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin,
+  `solution` text COLLATE utf8_bin,
+  `cvss_base_score` decimal(3,1),
+  `see_also` text COLLATE utf8_bin,
+  `cve` varchar(50) COLLATE utf8_bin,
+  `internal` varchar(10) COLLATE utf8_bin,
+  `external` varchar(10) COLLATE utf8_bin,
   `scanID` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -305,9 +304,9 @@ CREATE TABLE `webvulns` (
   `vulnerability` varchar(50) COLLATE utf8_bin NOT NULL,
   `findingID` int(3) NOT NULL,
   `severity` varchar(8) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
-  `remediation` text COLLATE utf8_bin NOT NULL,
-  `see_also` text COLLATE utf8_bin NOT NULL
+  `description` text COLLATE utf8_bin,
+  `remediation` text COLLATE utf8_bin,
+  `see_also` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
