31eb9f6f5681d6f440ccce1032ae0a8143e4ce6a
Lee Baird <leebaird@gmail.com>
2017-12-25 20:57:27 -0500

    Updated external links on Home page.

--- src/php/public/home.php
+++ src/php/public/home.php
@@ -13,7 +13,6 @@
         <ul>
             <li><a href="../includes/edit_account.php">Edit Account</a></li>
             <?php
-			
                 if ($_SESSION['user']['userRoleID']==1) {
             ?>
             <li><a href="../includes/users.php">Users</a></li>
@@ -29,7 +28,7 @@
         <h4>Tools</h4>
         <ul>
             <li><a href="https://www.acunetix.com/vulnerabilities/web/" target="_blank">acunetix</a></li>
-            <li><a href="https://portswigger.net/KnowledgeBase/Issues/" target="_blank">Burp Suite</a></li>
+            <li><a href="https://portswigger.net/kb/Issues/" target="_blank">Burp Suite</a></li>
             <li><a href="https://www.tenable.com/plugins/index.php?view=all" target="_blank">Nessus</a></li>
             <li><a href="https://www.rapid7.com/db/search" target="_blank">Nexpose</a></li>
             <li><a href="https://nmap.org/nsedoc/" target="_blank">nmap scripts</a></li>
@@ -37,8 +36,9 @@
 
         <h4>Additional Links</h4>
         <ul>
+            <li><a href="https://cwe.mitre.org/data/index.html" target="_blank">Common Weakness Enumeration</a></li>
             <li><a href="https://www.exploit-db.com" target="_blank">Exploit Database</a></li>
-            <li><a href="https://www.owasp.org/index.php/Category:OWASP_Top_Ten_Project#tab=OWASP_Top_10_for_2017_Release_Candidate_1" target="_blank">OWASP Top 10</a></li>
+            <li><a href="https://www.owasp.org/images/7/72/OWASP_Top_10-2017_%28en%29.pdf.pdf" target="_blank">OWASP Top 10</a></li>
             <li><a href="http://projects.webappsec.org/w/page/13246978/Threat%20Classification" target="_blank">WASC Threat Classification</a></li>
         </ul>
     </div>
